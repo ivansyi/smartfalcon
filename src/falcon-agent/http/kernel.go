8ef7342e88a1128f7007df5c8d81cdbd888107a2
Yi, Shunli <shunliyi@hotmail.com>
2016-12-14 13:36:40 +0800

    add some system stat

--- src/falcon-agent/http/kernel.go
+++ src/falcon-agent/http/kernel.go
@@ -23,8 +23,53 @@ func configKernelRoutes() {
 		AutoRender(w, data, err)
 	})
 
+	http.HandleFunc("/proc/kernel/file-max", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.KernelMaxFiles()
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/kernel/file-nr", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.KernelAllocateFiles()
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/kernel/procs", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.AllProcs()
+		AutoRender(w, data, err)
+	})
+
 	http.HandleFunc("/proc/kernel/version", func(w http.ResponseWriter, r *http.Request) {
 		data, err := sys.CmdOutNoLn("uname", "-r")
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/socket/tcp", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.TcpPorts()
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/socket/udp", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.UdpPorts()
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/socket/sum", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.SocketStatSummary()
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/netstat/tcp", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.Netstat("TcpExt")
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/netstat/ip", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.Netstat("IpExt")
+		AutoRender(w, data, err)
+	})
+
+	http.HandleFunc("/proc/meminfo", func(w http.ResponseWriter, r *http.Request) {
+		data, err := nux.MemInfo()
 		AutoRender(w, data, err)
 	})
 
